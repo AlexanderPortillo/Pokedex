6a42a515e81810fbfedf923e7ea9b03910c9337a
AlexanderPortillo <105524042+AlexanderPortillo@users.noreply.github.com>
2023-08-23 23:22:13 -0600

    Avance

---
+++ Js/data.js
@@ -0,0 +1,16 @@
+const contenido = document.getElementById('boxes');
+const datos = document.getElementById('data');
+
+contenido.addEventListener('click', (e) => {
+	if (e.target.closest('.box')) {
+		datos.classList.add('data--active');
+	}
+});
+
+datos.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	if (e.target.closest('button')) {
+		datos.classList.remove('data--active');
+	}
+});
